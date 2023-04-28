2f4e6f40e2ac3256cff920c0ea6f39481504f091
Ricardo <ric.asjunior@gmail.com>
2023-04-28 13:02:19 -0300

    refatorando

--- app/validacao.js
+++ app/validacao.js
@@ -2,19 +2,29 @@ function verificaSeOChutePossuiUmValorValido(chute){
     const numero = +chute;
 
     if (chuteForInvalido(numero)){
-    elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return
     }
 
-    if(numeroForMaiorQueOPermitido(numero)){
-
-        elementoChute.innerHTML += `<div>Valor inválido: Fale un numero
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale un numero
         enrtre ${menorValor} e ${maiorValor}</div> `
+        return
     }
 
     if (numero === numeroSecreto){
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
-            <h3>O número Secreto era ${numeroSecreto}
+            <h3>O número Secreto era ${numeroSecreto}</h3>
+        `
+    }else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O numero secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    }else{
+        elementoChute.innerHTML += `
+        <div>O numero secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
 
