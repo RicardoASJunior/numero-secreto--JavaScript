1cbab79263f16ac40639036efdb7ee1c8039718a
Ricardo <ric.asjunior@gmail.com>
2023-04-28 12:42:38 -0300

    Acrescentando a validação dos números

--- app/validacao.js
+++ app/validacao.js
@@ -1,3 +1,29 @@
 function verificaSeOChutePossuiUmValorValido(chute){
-    
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)){
+    elementoChute.innerHTML += '<div>Valor Inválido</div>'
+    }
+
+    if(numeroForMaiorQueOPermitido(numero)){
+
+        elementoChute.innerHTML += `<div>Valor inválido: Fale un numero
+        enrtre ${menorValor} e ${maiorValor}</div> `
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número Secreto era ${numeroSecreto}
+        `
+    }
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorQueOPermitido(numero){
+    return numero > maiorValor || numero < menorValor;
 }
